d4f4f49157db3ec2c693e0121c701aaf1c806a96
Mulangu Mundemba <mulanguk.mundemba@gmail.com>
2022-06-01 20:32:57 -0500

    Added a Text Widget as the child of a Center Widget

--- lib/main.dart
+++ lib/main.dart
@@ -102,6 +102,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Center(
+              child: Text('Hello World!'),
+            )
           ],
         ),
       ),
